333468391be0a599ab3fd05a14e59aa4b315d8bf
3103407342@qq.com <3103407342@qq.com>
2023-03-08 18:19:24 +0800

    fix: 若src目录存在则删除

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+
 /**
  * 引入问候依赖
  * 注意！！！！！
  * 如果想要使用commonjs模式，需要下载inquirer@8.0.0版本，高版本使用ESM格式
  * 
+ * chalk ==> chalk@4.0.0支持commonjs引入
  */
 const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
 const ejs = require('ejs')
+const chalk = require('chalk')
 
 
 /**
@@ -24,6 +27,10 @@ inquirer.prompt([
         default: 'my-app'   // 默认值
     }
 ]).then(answers => {
+
+    // 输出项目名称
+    const projectNameStyle = chalk.green.bold;
+    console.log('Your project name is ' + projectNameStyle(answers.name))
    
     /**
      * 模板文件目录
@@ -43,11 +50,23 @@ inquirer.prompt([
     fs.readdir(templeteURL, (err, files) =>{
         if(err) throw err;
 
-
         /**
          * fs.mkdirSync在根目录创建src文件夹
+         * 如果当前目录下有src则删除该文件夹
+         * 
+         * fs.existsSync 判断目录是否存在, 通过path.resolve拼接出src路径
          */
-        fs.mkdirSync(path.join(cwdUrl, 'src'))
+        const srcUrl = path.join(cwdUrl, 'src')
+
+        if(fs.existsSync(srcUrl)) {
+            /**
+             * 如果存在src文件夹则移除该文件夹
+             * rmSync 默认只能删除空目录，如果想要删除非空目录则需要加recursive字段
+             */
+            fs.rmSync(srcUrl, { recursive: true })
+        }
+
+        fs.mkdirSync(srcUrl)
 
         /**
          * files 文件地址数组
