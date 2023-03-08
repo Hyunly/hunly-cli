7cae92fb0ebc48bf96d8c973c3130f46c6927a17
3103407342@qq.com <3103407342@qq.com>
2023-03-08 11:55:21 +0800

    feat: 引入问题查询依赖inquirer

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log(process.argv[2])
+
+/**依赖
+ * 引入问候
+ */
+const inquirer = require('inquirer');
+
+/**
+ * inquire.protmpt： 设置需要向用户询问的信息。
+ * []：配置为一个数组，表示可以多次访问
+ */
+inquirer.prompt([
+    {
+        type: 'input',  // 类型为输入，type： input, number, confirm, list, checkbox ... 
+        name: 'name',   // key
+        message: 'Your Object-name',    // 提示信息
+        default: 'my-app'   // 默认值
+    }
+]).then(answers => {
+    console.log(answers)
+})
